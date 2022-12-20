8e89e99d8cbf450b3a60af4b3cf10e13e73bbbb8
Xiaotian Yang <xya34@uclive.ac.nz>
2022-12-20 16:18:58 +1300

    some initial commits. Added a Readme.md file

---
+++ generalindex_workflow/README.md
@@ -0,0 +1,2 @@
+# GeneralIndex-Workflow
+A workflow for the public resource known as 'The General Index'. The database consists of metadata, keywords and n-gram tables for a very large amount of scholarly publications.
--- generalindex_workflow/all_date_quality.sql
+++ generalindex_workflow/all_date_quality.sql
@@ -3,7 +3,7 @@
 --\pset format wrapped
 --\pset columns 20
 
-drop table meta_clean if exists;
+drop table if exists meta_clean;
 
 with clean_date as 
 (select  dkey,
@@ -41,60 +41,4 @@ where year is NULL
 group by year,pub_date
 order by Frequency desc;
 
-
---now producing summary queries 
---just extracting the year from the dates
---Aggregating year data
-select Year,count(title) as DocumentCounts 
-from meta_clean
-where year is not NULL
-group by Year
-order by Year asc;
---creating a date table for ease of use for other summary queries
-/*
---creating a new table of only recent data
-drop table metadata_recent;
-select *
-into metadata_recent 
-from  meta_clean as mr
-where date_part('year',current_date) - mr.year <= 50; --only looking at the past 50 years of dates
-
---summarising for a given author 
-
-select author,Year,count(title) as DocumentCounts
-from metadata_recent
-where author like ANY(ARRAY['%Pedro Ros Petrovick%']) -- like ALL for exlusive for multiple authors
-AND Year is not NULL
-group by author,Year
-order by author, Year asc;
-
---summarising for a particular title
-select title,Year,count(title) as DocumentCounts
-from metadata_recent
-where title like ANY(ARRAY['%rotary micromachines%'])
-and Year is not NULL
-group by title,Year
-order by title, Year asc;
-
---counting the number of authors per document
-select dkey,
-	SUBSTRING(author,0,10) as author_part,
-	(select count(*) from regexp_matches(author,';','g')) + 1 as AuthorCount
-from metadata_recent
-order by AuthorCount desc;
-
-
---remove meta_clean table
-drop table meta_clean;
-*/
-/*
---publications per author
-
-select author,count(title) as Frequency
-from
-clean_meta
-where Year is not NULL
-group by author
-order by Frequency desc;
-*/
 
