dd5137b91a3bdaf35dfc33c9a766149befab188a
Xiaotian Yang <xya34@uclive.ac.nz>
2023-01-04 14:57:09 +1300

    format transformer updated

--- generalindex_workflow/format_transformer.py
+++ generalindex_workflow/format_transformer.py
@@ -6,20 +6,8 @@ This file rewrites the a .sql.zip file into a csv equivalent
 import sys
 import subprocess
 import pkg_resources
+import concurrent.futures
 
-##install missing packages
-required = {'dataclasses','pyarrow','pandas'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-
-if missing:
-    for package in list(missing):
-        try:
-            dist = pkg_resources.get_distribution(package)
-            print('{} ({}) is installed'.format(dist.key, dist.version))
-        except pkg_resources.DistributionNotFound:
-            print('{} is NOT installed'.format(package))
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
 ##imports
 from dataclasses import dataclass,field
@@ -30,12 +18,13 @@ from more_itertools import chunked
 from tqdm import tqdm
 from enum import Enum
 import re
+import os
 
 def write_parquet_table(df,filepath=None):
     """Method writes dataframes in parquet format.
 
     This method is used to write pandas DataFrame as pyarrow Table in parquet format.
-    
+
     :param dataframe: pd.DataFrame to be written in parquet format.
     :param filepath: target file location for parquet file.
     :param writer: ParquetWriter object to write pyarrow tables in parquet format.
@@ -45,8 +34,9 @@ def write_parquet_table(df,filepath=None):
     writer.write_table(table=table)
     writer.close()
     
-def write_parquet_batch(df,filepath=None):
     
+def write_parquet_batch(df,filepath=None):
+
     batch = pa.RecordBatch.from_pandas(df)
     writer = pq.ParquetWriter(filepath, batch.schema)
     writer.write_batch(batch)
@@ -60,12 +50,15 @@ class Size(Enum):
 @dataclass
 class SQLDumpTransformer:
     file_name:str
+    file_size = 0
     data_start_idx = 0
     chunks_written = 0
+    
 
     def __post_init__(self):
         self.findDataStart()
-        
+        self.getOGFileSize()
+
     def findDataStart(self) -> int:
         '''returns the starting line number for the data in the file'''
         """opens the file in byte stream format"""
@@ -91,12 +84,18 @@ class SQLDumpTransformer:
             "insert_date": "date"
         }
         return d
+    
+    def getOGFileSize(self):
+        self.file_size = os.path.getsize(self.file_name)
+        
 
-    def writeToParquetBuffered(self,chunksize:int  = 240_000_000) -> None:
+    def writeToParquetBuffered(self,chunksize:int  = 1_024_000_000) -> None:
         """writes a file in chunks to format"""
         byteswritten = 0
         writer = None #initializing parquet writer
         chunk = [] ##initial chunk write
+        total_chunks = self.file_size // chunksize
+        
         #read the new encoded file with buffering in bytestream
         infile = open(self.file_name, "rb", buffering=Size.MEB.value)
         new_filename = self.file_name.split(".")[0] + ".parquet.gz"
@@ -117,15 +116,18 @@ class SQLDumpTransformer:
                         ##converting to parquet format
                         if self.chunks_written > 1:
                             df = pd.DataFrame(data=chunk,columns=colnames)
+                            new_filename = self.file_name.split(".")[0] + f"_{self.chunks_written}.parquet.gz"
                             write_parquet_batch(df,new_filename)
+
                         else:
+
                             colnames = list(self.getSchema().keys()) #obtain column names from schema
                             df = pd.DataFrame(data=chunk,columns=colnames)
                             write_parquet_table(df,new_filename)
                         byteswritten = 0 ##reset bytes written to start writing the next chunk                  >
                         chunk = [] #reinitialise chunk
                         self.chunks_written += 1
-                        print(f"{self.chunks_written} chunks written.")
+                        print(f"{self.chunks_written} of {total_chunks} chunks written.")
 
 
                     chunk.append(line.decode().split("\t")) ##adding list represention of data chunk
@@ -133,16 +135,34 @@ class SQLDumpTransformer:
         finally:
             infile.close()
         assert self.chunks_written > 0, "No chunks written. Please reduce chunk size"
+        
 
 def main():
-    ##parsing command line arguments
-    args = sys.argv
-    zip_filename = args[1] ##extract the first non-script argument from the list
-    #zip_filename = 'keywords_sample.sql.zip'
-    ft = SQLDumpTransformer(zip_filename)
-    ft.writeToParquetBuffered(chunksize=Size.MEB.value * 1024)
+   
+    ##install missing packages
+    required = {'dataclasses','pyarrow','pandas'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
 
+    if missing:
+        for package in list(missing):
+            try:
+                dist = pkg_resources.get_distribution(package)
+                print('{} ({}) is installed'.format(dist.key, dist.version))
+            except pkg_resources.DistributionNotFound:
+                print('{} is NOT installed'.format(package))
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
+    # define file name
+    file_name = sys.argv[1]
 
-if __name__ == "__main__":
+    # create SQLDumpTransformer instance
+    transformer = SQLDumpTransformer(file_name)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        # submit task to thread pool
+        future = executor.submit(transformer.writeToParquetBuffered)
+
+if __name__ == '__main__':
+    # use cores for multiprocessing
     main()
